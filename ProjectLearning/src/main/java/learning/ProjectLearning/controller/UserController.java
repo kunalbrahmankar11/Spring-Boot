dc561b04c951e11c3d35559682c2ba13a5b7fc44
krunalbrahmankar11 <krunalbrahmankar43@gmail.com>
2025-02-22 21:20:29 +0530

    initial commit

--- ProjectLearning/src/main/java/learning/ProjectLearning/controller/UserController.java
+++ ProjectLearning/src/main/java/learning/ProjectLearning/controller/UserController.java
@@ -3,6 +3,8 @@ package learning.ProjectLearning.controller;
 import learning.ProjectLearning.Entities.Blog;
 import learning.ProjectLearning.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,23 @@ public class UserController {
     public List<Blog> getUsers(){
         return userService.getAllUsers();
     }
-    @GetMapping("/blog/{id}")
-    public Blog show(@PathVariable String id){
-        int blogId = Integer.parseInt(id);
-        return userService.getUsers().get(blogId);
+
+    @GetMapping("/v1/{id}")
+    public ResponseEntity<Blog> show(@PathVariable String id) {
+        try {
+            int blogId = Integer.parseInt(id);
+            List<Blog> users = userService.getUsers();
+
+            if (users == null || blogId < 0 || blogId >= users.size()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+
+            return ResponseEntity.ok(users.get(blogId));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
+
 
 
 }
